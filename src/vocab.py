45db8f787cd250acb4cea70006efad0f8d39d9b6
nextBillyonair <wwatso13@jhu.edu>
2018-11-02 02:39:21 -0400

    Fix EOS SOS tokens, and usage case

--- src/vocab.py
+++ src/vocab.py
@@ -1,23 +1,24 @@
 import logging
 from glob import glob
 
-
+# Tokens
 SOS_TOKEN = "<SOS>"
 EOS_TOKEN = "<EOS>"
 
 SOS_INDEX = 0
 EOS_INDEX = 1
 
-MODERN_PATH='../../data/modern/'
-ORIGINAL_PATH='../../data/original/'
-PROCESSED_PATH='../../data/processed/'
+# Path to processed aligned data
+MODERN_PATH='../data/processed/modern.snt.aligned'
+ORIGINAL_PATH='../data/processed/original.snt.aligned'
+
 
 class Vocab:
     """This class handles the mapping between the words and their indices."""
     def __init__(self, lang_code):
         self.lang_code = lang_code
-        self.word2index = {}
-        self.word2count = {}
+        self.word2index = {SOS_TOKEN: SOS_INDEX, EOS_TOKEN: EOS_INDEX}
+        self.word2count = {SOS_TOKEN: 0, EOS_TOKEN: 0}
         self.index2word = {SOS_INDEX: SOS_TOKEN, EOS_INDEX: EOS_TOKEN}
         self.n_words = 2  # Count SOS and EOS
 
@@ -35,26 +36,23 @@ class Vocab:
             self.word2count[word] += 1
 
 
-def get_files(path=PROCESSED_PATH):
-    return glob(path+'*')
-
-
 def read_file(input_file):
     logging.info("Reading lines of %s...", input_file)
     return open(input_file).read().strip().split('\n')
 
 
-def make_vocabs(src_lang_code, tgt_lang_code, train_file):
+def make_vocabs(src_code, tgt_code, file_pair):
     """
     Creates the vocabs for each of the languages based on the training
     corpus.
     """
-    src_vocab = Vocab(src_lang_code)
-    tgt_vocab = Vocab(tgt_lang_code)
+    src_vocab = Vocab(src_code)
+    tgt_vocab = Vocab(tgt_code)
 
-    train_pairs = read_file(train_file)
+    src = read_file(file_pair[src_code])
+    tgt = read_file(file_pair[tgt_code])
 
-    for pair in train_pairs:
+    for pair in zip(src, tgt):
         src_vocab.add_sentence(pair[0])
         tgt_vocab.add_sentence(pair[1])
 
@@ -62,3 +60,18 @@ def make_vocabs(src_lang_code, tgt_lang_code, train_file):
     logging.info(f'{tgt_vocab.lang_code} tgt vocab size: {tgt_vocab.n_words}')
 
     return src_vocab, tgt_vocab
+
+
+if __name__ == '__main__':
+    # USEAGE
+    src_vocab, tgt_vocab = make_vocabs('Original', 'Modern',
+                                       {'Original':ORIGINAL_PATH,
+                                        'Modern':MODERN_PATH})
+
+    print(src_vocab.lang_code, src_vocab.n_words)
+    print(tgt_vocab.lang_code, tgt_vocab.n_words)
+
+    # 0 : 21079
+    print(src_vocab.word2index[SOS_TOKEN], src_vocab.word2count[SOS_TOKEN])
+    # 1 : 21079
+    print(src_vocab.word2index[EOS_TOKEN], src_vocab.word2count[EOS_TOKEN])
