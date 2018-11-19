3c68cd4d4c1876857553e0623f0256e307a3f3ae
Riley Scott <rscott39@jhu.edu>
2018-11-19 14:32:22 -0500

    fixed windows_experiments.cmd (changing attention mode dot -> general)

--- windows_experiments.cmd
+++ windows_experiments.cmd
@@ -3,8 +3,8 @@ python main.py --encoder-mode rnn --decoder-mode rnn --attention-mode none --use
 python main.py --encoder-mode gru --decoder-mode gru --attention-mode none --use-gpu > results/gru.txt
 python main.py --encoder-mode bigru --decoder-mode gru --attention-mode none --use-gpu > results/bigru.txt
 python main.py --encoder-mode bigru --decoder-mode gru --attention-mode concat --use-gpu > results/bigru_concat.txt
-python main.py --encoder-mode bigru --decoder-mode gru --attention-mode dot --use-gpu > results/bigru_dot.txt
+python main.py --encoder-mode bigru --decoder-mode gru --attention-mode general --use-gpu > results/bigru_dot.txt
 python main.py --encoder-mode bigru --decoder-mode gru --attention-mode concat --teacher-forcing-chance 0.5 --use-gpu > results/bigru_concat_50.txt
-python main.py --encoder-mode bigru --decoder-mode gru --attention-mode dot --teacher-forcing-chance 0.5 --use-gpu > results/bigru_dot_50.txt
+python main.py --encoder-mode bigru --decoder-mode gru --attention-mode general --teacher-forcing-chance 0.5 --use-gpu > results/bigru_dot_50.txt
 python main.py --encoder-mode bigru --decoder-mode gru --attention-mode concat --teacher-forcing-chance 0.5 --use-gpu > results/bigru_concat_100.txt
-python main.py --encoder-mode bigru --decoder-mode gru --attention-mode dot  --teacher-forcing-chance 0.5 --use-gpu > results/bigru_dot_100.txt
+python main.py --encoder-mode bigru --decoder-mode gru --attention-mode general  --teacher-forcing-chance 0.5 --use-gpu > results/bigru_dot_100.txt
